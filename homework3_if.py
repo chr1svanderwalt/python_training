d69bbe2841717328d24e9a04ad8c5757d8705ce6
chr1svanderwalt <45593240+chr1svanderwalt@users.noreply.github.com>
2019-03-02 20:15:07 +0200

    Update homework3_if.py

--- homework3_if.py
+++ homework3_if.py
@@ -1,7 +1,16 @@
+
 def compair(varA, varB, varC):
     if int(varA) == int(varB) or int(varA) == int(varC) or int(varB) == int(varC):
-        print('True')
+        return True
     else:
-        print('False')
+        return False
 
-compair(5,6,5)
+
+""" Test for function to determin if function work correctly """
+print(compair(5,5,7.5))
+
+print(compair(5000,6,5000))
+
+print(compair(5,654,654))
+
+print(compair(1,2,3))
